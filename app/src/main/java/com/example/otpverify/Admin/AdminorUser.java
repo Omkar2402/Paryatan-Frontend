f9d9c29e394415493b46105d0de979f32eb1e5df
Omkar2402 <khareomkar24@gmail.com>
2022-02-16 22:11:14 +0530

    Added cardviews

---
+++ app/src/main/java/com/example/otpverify/Admin/AdminorUser.java
@@ -0,0 +1,16 @@
+package com.example.otpverify.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.otpverify.R;
+
+public class AdminorUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminor_user);
+    }
+}
--- app/src/main/java/com/example/otpverify/MenuActivity.java
+++ app/src/main/java/com/example/otpverify/MenuActivity.java
@@ -22,20 +22,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        camera = (ImageButton)findViewById(R.id.cambutton);
-        camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Intent intent = new Intent();
-                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivity(intent);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+//        camera = (ImageButton)findViewById(R.id.cambutton);
+//        camera.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    Intent intent = new Intent();
+//                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivity(intent);
+//                }
+//                catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
 
     }
 
