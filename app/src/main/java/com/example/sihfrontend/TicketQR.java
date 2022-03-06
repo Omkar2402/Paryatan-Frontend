12df87a474cc88e910b2fb2ed9bbeee7c6587dd8
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-03-06 09:01:48 +0530

    Modified TicketQR

--- app/src/main/java/com/example/sihfrontend/TicketQR.java
+++ app/src/main/java/com/example/sihfrontend/TicketQR.java
@@ -7,13 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sihfrontend.user.ticketInfo;
 
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
 
 public class TicketQR extends AppCompatActivity {
 
@@ -23,6 +29,8 @@ public class TicketQR extends AppCompatActivity {
     private RecyclerView recyclerViewQR;
     private TicketQRAdapter ticketQRAdapter;
     private String monumentName;
+    private Button downloadDetails;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,11 @@ public class TicketQR extends AppCompatActivity {
         tvMonumentName = findViewById(R.id.tvMonumentNameQR);
         tvDateOfVisit = findViewById(R.id.tvDateofVisit);
         recyclerViewQR = findViewById(R.id.ticketQR_recycler_view);
+        downloadDetails = findViewById(R.id.btnDownloadDetails);
+        progressBar = findViewById(R.id.ProgressBarQR);
+
+        progressBar.setVisibility(View.VISIBLE);
+        fetchQRImage();
 
         Intent intent = getIntent();
         Bundle args = intent.getBundleExtra("bundle");
@@ -46,7 +59,29 @@ public class TicketQR extends AppCompatActivity {
         recyclerViewQR.setLayoutManager(linearLayoutManager);
         recyclerViewQR.setAdapter(ticketQRAdapter);
 
+        downloadDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(TicketQR.this,"Download Details",Toast.LENGTH_LONG).show();
+                /*
+                Option 1:
+                    should download all the details of current activity screen and save in pdf
+                Option 2:
+                    save QR image from backend server in pdf with little or no text(easy)
+                    https://stackoverflow.com/questions/8644459/how-to-convert-byte-array-to-pdf-file-in-android
+                */
 
+
+
+            }
+        });
+
+    }
+
+    private void fetchQRImage() {
+
+        OkHttpClient client = new OkHttpClient();
+        //Not calculated bill yet
 
     }
 }
