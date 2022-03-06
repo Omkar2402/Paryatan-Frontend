dbc005a656a4e88c110824dbfa0497ff0bcd86d4
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-03-06 08:34:34 +0530

    Added TicketQR recyclerview

--- app/src/main/java/com/example/sihfrontend/TicketQR.java
+++ app/src/main/java/com/example/sihfrontend/TicketQR.java
@@ -1,14 +1,52 @@
 package com.example.sihfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.sihfrontend.user.ticketInfo;
+
+import java.util.ArrayList;
 
 public class TicketQR extends AppCompatActivity {
+
+    private ImageView imgQR;
+    private TextView tvMonumentName;
+    private TextView tvDateOfVisit;
+    private RecyclerView recyclerViewQR;
+    private TicketQRAdapter ticketQRAdapter;
+    private String monumentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_qr);
+        imgQR = findViewById(R.id.imgQRCode);
+        tvMonumentName = findViewById(R.id.tvMonumentNameQR);
+        tvDateOfVisit = findViewById(R.id.tvDateofVisit);
+        recyclerViewQR = findViewById(R.id.ticketQR_recycler_view);
+
+        Intent intent = getIntent();
+        Bundle args = intent.getBundleExtra("bundle");
+        ArrayList<ticketInfo>  ticketInfoArrayList = (ArrayList<ticketInfo>) args.getSerializable("arrayList");
+        monumentName = intent.getStringExtra("monumentName");
+
+        ticketQRAdapter = new TicketQRAdapter(TicketQR.this,ticketInfoArrayList){
+
+        };
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(TicketQR.this,LinearLayoutManager.VERTICAL,false);
+        recyclerViewQR.setLayoutManager(linearLayoutManager);
+        recyclerViewQR.setAdapter(ticketQRAdapter);
+
+
+
     }
 }
