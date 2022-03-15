e3d8b2f5a9e6a5373ab4577b8d7ad6936fde3ee6
aditinikam <aditirnikam21@gmail.com>
2022-03-15 19:22:53 +0530

    Bank Details added

---
+++ app/src/main/java/com/example/sihfrontend/admin/InVerificationProcess.java
@@ -0,0 +1,14 @@
+package com.example.sihfrontend.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InVerificationProcess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_verification_process);
+    }
+}
