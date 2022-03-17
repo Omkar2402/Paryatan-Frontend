41b4a922bcf21fbec3be2a6e04c8dcc96b647820
Omkar Khare <khareomkar24@gmail.com>
2022-03-17 20:27:53 +0530

    Working

--- app/src/main/java/com/example/sihfrontend/MainActivity.java
+++ app/src/main/java/com/example/sihfrontend/MainActivity.java
@@ -104,16 +104,17 @@ public class MainActivity extends AppCompatActivity {
                                     MainActivity.this.runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
-                                            if(otp!=null){
+                                            if (otp.equals("null")) {
+                                                Toast.makeText(getApplicationContext(), "This email id has been blocked due to fake booking of tickets...", Toast.LENGTH_SHORT).show();
+                                            }
+                                            else if(otp!=null){
                                                 Intent verifyIntent = new Intent(getApplicationContext(), OTPVerification.class);
                                                 verifyIntent.putExtra("email",email.getText().toString());
                                                 verifyIntent.putExtra("otp",otp);
                                                 startActivity(verifyIntent);
 
                                             }
-                                            else if (otp==null) {
-                                                Toast.makeText(getApplicationContext(), "This email id has been blocked due to fake booking of tickets...", Toast.LENGTH_SHORT).show();
-                                            }
+
                                             else{
                                                 Toast.makeText(MainActivity.this,"OTP not sent",Toast.LENGTH_SHORT).show();
                                             }
