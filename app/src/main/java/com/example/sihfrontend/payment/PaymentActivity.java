04d153ff30de78b80834c238ab8cbacb43cbe0d8
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-04-02 12:42:39 +0530

    Payment integration done

--- app/src/main/java/com/example/sihfrontend/payment/PaymentActivity.java
+++ app/src/main/java/com/example/sihfrontend/payment/PaymentActivity.java
@@ -1,135 +1,103 @@
 package com.example.sihfrontend.payment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.sihfrontend.R;
-import com.shreyaspatil.EasyUpiPayment.EasyUpiPayment;
-import com.shreyaspatil.EasyUpiPayment.listener.PaymentStatusListener;
-import com.shreyaspatil.EasyUpiPayment.model.TransactionDetails;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import org.json.JSONObject;
 
-public class PaymentActivity extends AppCompatActivity  implements PaymentStatusListener {
+import java.util.ArrayList;
 
-    private EditText amountEdt, upiEdt, nameEdt, descEdt;
-    private TextView transactionDetailsTV;
+public class PaymentActivity extends AppCompatActivity implements PaymentResultListener {
+
+    private EditText amountEdt;
+    private Button payBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
         amountEdt = findViewById(R.id.idEdtAmount);
-        upiEdt = findViewById(R.id.idEdtUpi);
-        nameEdt = findViewById(R.id.idEdtName);
-        descEdt = findViewById(R.id.idEdtDescription);
-        Button makePaymentBtn = findViewById(R.id.idBtnMakePayment);
-        transactionDetailsTV = findViewById(R.id.idTVTransactionDetails);
+        payBtn = findViewById(R.id.idBtnPay);
 
-        // on below line we are getting date and then we are setting this date as transaction id.
-        Date c = Calendar.getInstance().getTime();
-        SimpleDateFormat df = new SimpleDateFormat("ddMMyyyyHHmmss", Locale.getDefault());
-        String transcId = df.format(c);
-
-        // on below line we are adding click listener for our payment button.
-        makePaymentBtn.setOnClickListener(new View.OnClickListener() {
+        payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // on below line we are getting data from our edit text.
-                String amount = amountEdt.getText().toString();
-                String upi = upiEdt.getText().toString();
-                String name = nameEdt.getText().toString();
-                String desc = descEdt.getText().toString();
-                // on below line we are validating our text field.
-                if (TextUtils.isEmpty(amount) && TextUtils.isEmpty(upi) && TextUtils.isEmpty(name) && TextUtils.isEmpty(desc)) {
-                    Toast.makeText(PaymentActivity.this, "Please enter all the details..", Toast.LENGTH_SHORT).show();
-                } else {
-                    // if the edit text is not empty then
-                    // we are calling method to make payment.
-                    makePayment(amount, upi, name, desc, transcId);
+                if(amountEdt.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter valid amount",Toast.LENGTH_LONG).show();
+                }else{
+                    String samount = amountEdt.getText().toString();
+                    int amount = Math.round(Float.parseFloat(samount)*100);
+
+                    Checkout checkout = new Checkout();
+
+                    checkout.setKeyID("rzp_live_dSG7KmGC5Ox5tS");
+
+                    checkout.setImage(R.drawable.heritage_logo);
+
+                    JSONObject object = new JSONObject();
+
+                    try {
+
+                        // to put name
+                        object.put("name", "GOD");
+
+                        // put description
+                        object.put("description", "Test payment");
+
+                        // to set theme color
+                        object.put("theme.color", "");
+
+                        // put the currency
+                        object.put("currency", "INR");
+
+                        // put amount
+                        object.put("amount", amount);
+
+                        // put mobile number
+                        object.put("prefill.contact", "8237345685");
+
+                        // put email
+                        object.put("prefill.email", "shivamvermasv380@gmail.com");
+
+                        // open razorpay to checkout activity
+                        checkout.open(PaymentActivity.this, object);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+
                 }
+
+
             }
         });
-    }
 
-    private void makePayment(String amount, String upi, String name, String desc, String transactionId) {
-        // on below line we are calling an easy payment method and passing
-        // all parameters to it such as upi id,name, description and others.
-        final EasyUpiPayment easyUpiPayment = new EasyUpiPayment.Builder()
-                .with(this)
-                // on below line we are adding upi id.
-                .setPayeeVpa(upi)
-                // on below line we are setting name to which we are making oayment.
-                .setPayeeName(name)
-                // on below line we are passing transaction id.
-                .setTransactionId(transactionId)
-                // on below line we are passing transaction ref id.
-                .setTransactionRefId(transactionId)
-                // on below line we are adding description to payment.
-                .setDescription(desc)
-                // on below line we are passing amount which is being paid.
-                .setAmount(amount)
-                // on below line we are calling a build method to build this ui.
-                .build();
-        // on below line we are calling a start
-        // payment method to start a payment.
-        easyUpiPayment.startPayment();
-        // on below line we are calling a set payment
-        // status listener method to call other payment methods.
-        easyUpiPayment.setPaymentStatusListener(PaymentActivity.this);
+
     }
 
     @Override
-    public void onTransactionCompleted(TransactionDetails transactionDetails) {
-        // on below line we are getting details about transaction when completed.
-        String transcDetails = transactionDetails.getStatus().toString() + "\n" + "Transaction ID : " + transactionDetails.getTransactionId();
-        transactionDetailsTV.setVisibility(View.VISIBLE);
-        // on below line we are setting details to our text view.
-        transactionDetailsTV.setText(transcDetails);
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(getApplicationContext(),"Payment successfull!!",Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onTransactionSuccess() {
-        // this method is called when transaction is successful and we are displaying a toast message.
-        Toast.makeText(this, "Transaction successfully completed..", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onTransactionSubmitted() {
-        // this method is called when transaction is done
-        // but it may be successful or failure.
-        Log.e("TAG", "TRANSACTION SUBMIT");
-    }
-
-    @Override
-    public void onTransactionFailed() {
-        // this method is called when transaction is failure.
-        Toast.makeText(this, "Failed to complete transaction", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onTransactionCancelled() {
-        // this method is called when transaction is cancelled.
-        Toast.makeText(this, "Transaction cancelled..", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onAppNotFound() {
-        // this method is called when the users device is not having any app installed for making payment.
-        Toast.makeText(this, "No app found for making transaction..", Toast.LENGTH_SHORT).show();
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(getApplicationContext(),"Payment failed!!",Toast.LENGTH_LONG).show();
     }
 }
