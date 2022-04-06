e9c9d5b34a1b175c344a76e267bef3ca906c29fc
Omkar Khare <khareomkar24@gmail.com>
2022-04-06 12:32:29 +0530

    User livestream done

--- app/src/main/java/com/example/sihfrontend/streaming/ViewStream.java
+++ app/src/main/java/com/example/sihfrontend/streaming/ViewStream.java
@@ -1,7 +1,9 @@
 package com.example.sihfrontend.streaming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import com.bambuser.broadcaster.BroadcastPlayer;
 import com.bambuser.broadcaster.PlayerState;
 import com.bambuser.broadcaster.SurfaceViewWithAutoAR;
 import com.example.sihfrontend.R;
+import com.example.sihfrontend.user.monument.MonumentDescription;
 
 import java.io.IOException;
 
@@ -25,179 +28,19 @@ import android.view.MotionEvent;
 
 public class ViewStream extends AppCompatActivity {
 
-    SurfaceViewWithAutoAR mVideoSurface;
-    TextView mPlayerStatusTextView;
-    BroadcastPlayer mBroadcastPlayer;
-    Display mDefaultDisplay;
-    BroadcastPlayer mBroadcastPlayerObserver;
-    MediaController mMediaController = null;
-
-
-    final OkHttpClient mOkHttpClient = new OkHttpClient();
-    View mPlayerContentView;
-    private static final String APPLICATION_ID = "17LguohzblGi0utJGc2qtw";
-    private  static  final String API_KEY = "npqKVJ8hgjuX3fiShUvsG";
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mOkHttpClient.dispatcher().cancelAll();
-        mVideoSurface = null;
-        if (mBroadcastPlayer != null)
-            mBroadcastPlayer.close();
-        mBroadcastPlayer = null;
-        if (mMediaController != null)
-            mMediaController.hide();
-        mMediaController = null;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-
-        if (ev.getActionMasked() == MotionEvent.ACTION_UP && mBroadcastPlayer != null && mMediaController != null) {
-            PlayerState state = mBroadcastPlayer.getState();
-            if (state == PlayerState.PLAYING ||
-                    state == PlayerState.BUFFERING ||
-                    state == PlayerState.PAUSED ||
-                    state == PlayerState.COMPLETED) {
-                if (mMediaController.isShowing())
-                    mMediaController.hide();
-                else
-                    mMediaController.show();
-            } else {
-                mMediaController.hide();
-            }
-        }
-
-        return false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mVideoSurface = findViewById(R.id.videoSurfaceView);
-        mPlayerStatusTextView.setText("Loading latest broadcast");
-        getLatestResourceUri();
-    }
-
-    private void getLatestResourceUri() {
-        Request request = new Request.Builder()
-                .url("https://api.bambuser.com/broadcasts")
-                .addHeader("Accept", "application/vnd.bambuser.v1+json")
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization","Bearer "+API_KEY)
-                .get()
-                .build();
-
-        mOkHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                ViewStream.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(mPlayerStatusTextView!=null){
-                            mPlayerStatusTextView.setText("Http Exception:"+e);
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String body = response.body().string();
-                String resourceUri = null;
-                try {
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                final  String uri = resourceUri;
-                ViewStream.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        initPlayer(uri);
-                    }
-                });
-            }
-        });
-    }
-
-    private void initPlayer(String resourceUri) {
-        if(resourceUri==null){
-            if(mPlayerStatusTextView!=null)
-                mPlayerStatusTextView.setText("Could not get info about latest broadcast");
-            return;
-        }
-        if(mVideoSurface==null){
-            return;
-        }
-        if (mBroadcastPlayer != null)
-            mBroadcastPlayer.close();
-        //mBroadcastPlayer = new BroadcastPlayer(this, resourceUri, APPLICATION_ID, mBroadcastPlayerObserver);
-        mBroadcastPlayer.setSurfaceView(mVideoSurface);
-        mBroadcastPlayer.load();
-    }
-
-    private Point getScreenSize() {
-        if (mDefaultDisplay == null)
-            mDefaultDisplay = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        try {
-            // this is officially supported since SDK 17 and said to work down to SDK 14 through reflection,
-            // so it might be everything we need.
-            mDefaultDisplay.getClass().getMethod("getRealSize", Point.class).invoke(mDefaultDisplay, size);
-        } catch (Exception e) {
-            // fallback to approximate size.
-            mDefaultDisplay.getSize(size);
-        }
-        return size;
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stream);
-        mVideoSurface = findViewById(R.id.videoSurfaceView);
-        mPlayerStatusTextView = findViewById(R.id.PlayerStatusTextView);
-
-        mDefaultDisplay = getWindowManager().getDefaultDisplay();
-
-//        mPlayerContentView = findViewById(R.id.PlayerContentView);
-        BroadcastPlayer.Observer mBroadcastPlayerObserver = new BroadcastPlayer.Observer() {
-            @Override
-            public void onStateChange(PlayerState playerState) {
-                if(mPlayerStatusTextView!=null){
-                    mPlayerStatusTextView.setText("Status:"+playerState);
-                }
-                if (playerState == PlayerState.PLAYING || playerState == PlayerState.PAUSED || playerState == PlayerState.COMPLETED) {
-                    if (mMediaController == null && mBroadcastPlayer != null && !mBroadcastPlayer.isTypeLive()) {
-                        mMediaController = new MediaController(ViewStream.this);
-                        mMediaController.setAnchorView(mPlayerContentView);
-                        mMediaController.setMediaPlayer(mBroadcastPlayer);
-                    }
-                    if (mMediaController != null) {
-                        mMediaController.setEnabled(true);
-                        mMediaController.show();
-                    }
-                } else if (playerState == PlayerState.ERROR || playerState == PlayerState.CLOSED) {
-                    if (mMediaController != null) {
-                        mMediaController.setEnabled(false);
-                        mMediaController.hide();
-                    }
-                    mMediaController = null;
-                }
-            }
-
-            @Override
-            public void onBroadcastLoaded(boolean live, int width, int height) {
-                Point size = getScreenSize();
-                float screenAR = size.x / (float) size.y;
-                float videoAR = width / (float) height;
-                float arDiff = screenAR - videoAR;
-                mVideoSurface.setCropToParent(Math.abs(arDiff) < 0.2);
-            }
-        };
+        try {
+            String url = "https://cdn.bambuser.net/player/?resourceUri=https%3A%2F%2Fcdn.bambuser.net%2Fgroups%2F105119%2Fbroadcasts%3Fby_authors%3D%26title_contains%3D%26has_any_tags%3D%26has_all_tags%3D%26da_id%3D4da957b0-c068-f529-1f39-e3136f98aa82%26da_timestamp%3D1649128470%26da_signature_method%3DHMAC-SHA256%26da_ttl%3D0%26da_static%3D1%26da_signature%3D71eae07c4f9d566fd961d2d5743700c7a3da83a4dee497f450ce1885c0536cbd";
+            //String url = "http://www.google.com";
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(ViewStream.this, Uri.parse(url));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
